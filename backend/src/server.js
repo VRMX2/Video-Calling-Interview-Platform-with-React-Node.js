5a92388f0509006d6bf2c38e88d3a858db9a41b7
VRMX2 <lehcengrissi@gmail.com>
2025-10-31 07:29:42 -0700

    new commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,13 @@
 import express from "express";
-import "dotenv/config";
+import {ENV} from "./lib/env.js";
 
-const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.get("/",(req,res)=>{
+
+
+
+app.get("/health", (req, res) => {
     res.status(200).json({msg:"success from backend"});
 });
 
-app.listen(3000,()=>console.log(`server is running on port ${PORT}`));
+app.listen(3000,()=>console.log(`server is running on port ${ENV.PORT}`));
